a83d0e868c02791b98e5f18ae758ac249773e161
jinohpark <jinoh1030@naver.com>
2024-11-14 19:07:58 +0900

    hit_wall

--- implements.py
+++ implements.py
@@ -95,10 +95,15 @@ class Ball(Basic):
     def hit_wall(self):
         # ============================================
         # TODO: Implement a service that bounces off when the ball hits the wall
-        pass
         # 좌우 벽 충돌
-        
+        if self.rect.left <= 0 or self.rect.right >= config.display_dimension[0]:
+            self.dir = 180 - self.dir  # 좌우 벽에서 반사
+            self.rect.clamp_ip(Rect(0, 0, config.display_dimension[0], config.display_dimension[1]))
+
         # 상단 벽 충돌
+        if self.rect.top <= 0:
+            self.dir = -self.dir  # 상단 벽에서 반사
+            self.rect.clamp_ip(Rect(0, 0, config.display_dimension[0], config.display_dimension[1]))
     
     def alive(self):
         # ============================================
